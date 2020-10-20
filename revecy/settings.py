5c373ead30db4c3a74bfc26e143d14a6272d605f
tahirs95 <tahirs95@hotmail.com>
2020-10-20 22:33:52 +0500

    Added ip in allowed hosts.

--- revecy/settings.py
+++ revecy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(chot82$ug2=s=xv%_^#2&ai+u@er3=8pmz+5sm+4(5+%to^rt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.138.158.185']
 
 
 # Application definition

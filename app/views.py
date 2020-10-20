42ffa90b767bde0de72d8cc3711eb1cab55f3208
tahirs95 <tahirs95@hotmail.com>
2020-10-20 19:33:27 +0500

    First commit.

---
+++ app/views.py
@@ -0,0 +1,49 @@
+import json
+import random
+import string
+from datetime import datetime
+import math
+import numpy as np
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.mail import send_mail
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.csrf import csrf_exempt
+
+def home(request):
+    return render(request, "home.html")
+
+def email(subject, message, recipient_list):
+    email_from = settings.EMAIL_HOST_USER
+    send_mail(subject, message, email_from, recipient_list)
+
+@csrf_exempt
+def send_email(request, *args, **kwargs):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        name = body['name']
+        phone = body['phone']
+        email = body['email']
+        message = body['message']
+        message = "Name:{}. Phone:{}. Email:{}. Message:{}"
+        email("Registration Completed", message, ["tahirs95@hotmail.com"])
+        return JsonResponse({"message":"only POST request is entertained"})
+
+
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def my_view(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        name = body['name']
+        phone = body['phone']
+        user_email = body['email']
+        message = body['message']
+        message = "Name:{}. Phone:{}. Email:{}. Message:{}".format(name, phone, user_email, message)
+        email("Registration Completed", message, ["tahirs95@hotmail.com"])
+        return JsonResponse({"message":"only POST request is entertained"})
